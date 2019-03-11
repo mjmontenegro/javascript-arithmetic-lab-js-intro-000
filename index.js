f5cd4b6fb774bb76e15cd547f1467bcadf78f393
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-11 20:54:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divinde(a, b) {
+  return a / b;
+}
+function inc(n) {
+  return ++n;
+}
+function dec(n) {
+  return --n;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function perserveDecimal(n) {
+  return parseFloat(n);
+}
